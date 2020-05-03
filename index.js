838c7972e271823a25b9954601e2ac5b94ffcae1
KatalinaNadasi <katalinanadasi@gmail.com>
2020-05-03 13:59:01 +0200

    Catch user name with a cookie and display it

--- index.js
+++ index.js
@@ -1,19 +1,31 @@
 const express = require('express')
-const app = express()
+const cookieParser = require('cookie-parser')
 const port = 4400
+
+const app = express()
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(cookieParser())
+
 
 app.set('view engine', 'pug')
 
 app.get('/', (req, res) => {
-  res.render('layout')
+  res.render('hello')
 })
 
 app.get('/hello', (req, res) => {
-  res.render('hello')
+  const name = req.cookies.username;
+  if(name) {
+    res.render('hello_response')
+  } else {
+    res.render('layout');
+  }
 })
 
 app.post('/hello', (req, res) => {
-  res.render('hello')
+  res.cookie('username', req.body.username)
+  res.render('hello_response', { name: req.body.username })
 })
 
 app.listen(port, () => console.log(`Listening on port ${port}`))
